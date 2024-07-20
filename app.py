1a2ba20cebb086a92d75d605cf8fa33c6e8dd87b
Priyanshu Upadhyay <68186246+Quantum-P@users.noreply.github.com>
2024-07-20 15:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,46 +47,47 @@ def main():
     """
     st.markdown(html_temp,unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1, 1, 1], gap="large")
-    with col1 :
+    with col1:
         st.markdown("### Mean Values")
-        radius_mean = st.number_input("radius_mean",)
-        texture_mean = st.number_input("texture_mean",)
-        perimeter_mean = st.number_input("perimeter_mean",)
-        area_mean = st.number_input("area_mean",)
-        smoothness_mean = st.number_input("smoothness_mean",)
-        compactness_mean = st.number_input("compactness_mean",)
-        concavity_mean = st.number_input("concavity_mean",)
-        concave_points_mean = st.number_input("concave_points_mean",)
-        symmetry_mean = st.number_input("symmetry_mean",)
-        fractal_dimension_mean = st.number_input("fractal_dimension_mean")
-        
-    with col2 :
-        st.markdown("### SE Values")
-        radius_se = st.number_input("radius_se",)
-        texture_se = st.number_input("texture_se",)
-        perimeter_se = st.number_input("perimeter_se",)
-        area_se = st.number_input("area_se",)
-        smoothness_se = st.number_input("smoothness_se",)
-        compactness_se = st.number_input("compactness_se",)
-        concavity_se = st.number_input("concavity_se",)
-        concave_points_se = st.number_input("concave_points_se",)
-        symmetry_se = st.number_input("symmetry_se",)
-        fractal_dimension_se = st.number_input("fractal_dimension_se",)
-        
-    with col3 :
-        st.markdown("### Worst Values")
-        radius_worst = st.number_input("radius_worst",)
-        texture_worst = st.number_input("texture_worst",)
-        perimeter_worst = st.number_input("perimeter_worst",)
-        area_worst = st.number_input("area_worst",)
-        smoothness_worst = st.number_input("smoothness_worst",)
-        compactness_worst = st.number_input("compactness_worst",)
-        concavity_worst = st.number_input("concavity_worst",)
-        concave_points_worst = st.number_input("concave_points_worst",)
-        symmetry_worst = st.number_input("symmetry_worst",)
-        fractal_dimension_worst = st.number_input("fractal_dimension_worst",)
-        
+        radius_mean = st.number_input("radius_mean", min_value=6.981, max_value=28.11)
+        texture_mean = st.number_input("texture_mean", min_value=9.71, max_value=39.28)
+        perimeter_mean = st.number_input("perimeter_mean", min_value=43.79, max_value=188.5)
+        area_mean = st.number_input("area_mean", min_value=143.5, max_value=2501.0)
+        smoothness_mean = st.number_input("smoothness_mean", min_value=0.05263, max_value=0.1634)
+        compactness_mean = st.number_input("compactness_mean", min_value=0.01938, max_value=0.3454)
+        concavity_mean = st.number_input("concavity_mean", min_value=0.0, max_value=0.4268)
+        concave_points_mean = st.number_input("concave_points_mean", min_value=0.0, max_value=0.2012)
+        symmetry_mean = st.number_input("symmetry_mean", min_value=0.106, max_value=0.304)
+        fractal_dimension_mean = st.number_input("fractal_dimension_mean", min_value=0.04996, max_value=0.09744)
     
+    # Second column inputs
+    with col2:
+        st.markdown("### SE Values")
+        radius_se = st.number_input("radius_se", min_value=0.1115, max_value=2.873)
+        texture_se = st.number_input("texture_se", min_value=0.3602, max_value=4.885)
+        perimeter_se = st.number_input("perimeter_se", min_value=0.757, max_value=21.98)
+        area_se = st.number_input("area_se", min_value=6.802, max_value=542.2)
+        smoothness_se = st.number_input("smoothness_se", min_value=0.001713, max_value=0.03113)
+        compactness_se = st.number_input("compactness_se", min_value=0.002252, max_value=0.1354)
+        concavity_se = st.number_input("concavity_se", min_value=0.0, max_value=0.396)
+        concave_points_se = st.number_input("concave_points_se", min_value=0.0, max_value=0.05279)
+        symmetry_se = st.number_input("symmetry_se", min_value=0.007882, max_value=0.07895)
+        fractal_dimension_se = st.number_input("fractal_dimension_se", min_value=0.000895, max_value=0.02984)
+    
+    # Third column inputs
+    with col3:
+        st.markdown("### Worst Values")
+        radius_worst = st.number_input("radius_worst", min_value=7.93, max_value=36.04)
+        texture_worst = st.number_input("texture_worst", min_value=12.02, max_value=49.54)
+        perimeter_worst = st.number_input("perimeter_worst", min_value=50.41, max_value=251.2)
+        area_worst = st.number_input("area_worst", min_value=185.2, max_value=4254.0)
+        smoothness_worst = st.number_input("smoothness_worst", min_value=0.07117, max_value=0.2226)
+        compactness_worst = st.number_input("compactness_worst", min_value=0.02729, max_value=1.058)
+        concavity_worst = st.number_input("concavity_worst", min_value=0.0, max_value=1.252)
+        concave_points_worst = st.number_input("concave_points_worst", min_value=0.0, max_value=0.291)
+        symmetry_worst = st.number_input("symmetry_worst", min_value=0.1565, max_value=0.6638)
+        fractal_dimension_worst = st.number_input("fractal_dimension_worst", min_value=0.05504, max_value=0.2075)
+        
 
     result = ""
     if st.button("Predict"):
